e6e0fa9581ba3dc7c28077c7bbd17cc7b178d0fc
nguye3hg <88401567+nguye3hg@users.noreply.github.com>
2023-07-12 21:55:59 +0700

    update3

--- .next/static/webpack/webpack.7545947c3d2baf0c.hot-update.js
+++ .next/static/webpack/webpack.7545947c3d2baf0c.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "94bd2ebb73626773"; }
+/******/ 	__webpack_require__.h = function() { return "a9f00ad945766847"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
